b4c471e0141e7c6bd03aa0d771b59af70973dfe4
Voitenko Illia <79865476+IlyaVoitenko@users.noreply.github.com>
2025-07-06 10:36:52 +0200

    added viteImagemin and changes to tsconfigs

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,6 @@
+import Main from "../components/Main";
+
+export const MainPage = () => {
+  return <Main />;
+};
+export default MainPage;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import viteImagemin from "vite-plugin-imagemin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      pngquant: {
+        quality: [0.65, 0.8],
+        speed: 3,
+      },
+      mozjpeg: {
+        quality: 75,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: "removeViewBox",
+            active: false,
+          },
+          {
+            name: "removeEmptyAttrs",
+            active: false,
+          },
+        ],
+      },
+      webp: {
+        quality: 75,
+      },
+    }),
+  ],
+});
