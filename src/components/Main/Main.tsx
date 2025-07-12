63bd57b92ad69e3b4daa645542fc477e9d176ce6
Voitenko Illia <79865476+IlyaVoitenko@users.noreply.github.com>
2025-07-12 22:33:34 +0200

    try to fix a bug

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,14 +1,17 @@
 import Header from "../Header";
 import "../../styles/components/_main.scss";
-import vf from "../../assets/CarMainFull1x.png";
+import c from "../../assets/CarMainFull1x.png";
 
 const Main = () => {
   return (
     <main className="page">
       <Header />
       <div className="drivingContainer">
-        <div className="pc1">
-          <img src={vf} alt="" />
+        <picture className="pc1"></picture>
+        <picture className="pc2"></picture>
+        <div className="block1">
+          <div className="rside">{/* <img src={c} alt="" /> */}</div>
+          <div className="lside"></div>
         </div>
       </div>
     </main>
