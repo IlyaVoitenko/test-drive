70822db123db61fdc786b8349b9ed52918ead8f4
Voitenko Illia <79865476+IlyaVoitenko@users.noreply.github.com>
2025-07-16 16:46:57 +0200

    renamed img and made new semantic html

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,17 +1,32 @@
 import Header from "../Header";
 import "../../styles/components/_main.scss";
-import c from "../../assets/CarMainFull1x.png";
 
 const Main = () => {
   return (
     <main className="page">
       <Header />
       <div className="drivingContainer">
-        <picture className="pc1"></picture>
-        <picture className="pc2"></picture>
-        <div className="block1">
-          <div className="rside">{/* <img src={c} alt="" /> */}</div>
-          <div className="lside"></div>
+        <div className="pictureLeft"></div>
+        <div className="pictureRight">
+          <picture className="carPicture">
+            <source
+              srcSet="../../assets/CarMain_2x.webp 2x, ../../assets/CarMain_3x.webp 3x"
+              type="image/webp"
+            />
+            <img
+              src="../../assets/CarMain.jpg"
+              alt="Car"
+              height={824}
+              width={1435}
+            />
+          </picture>
+        </div>
+        <div className="contentContainer">
+          <h1 className="title">Driving Redefined</h1>
+          <p className="subtitle">
+            Our impressive line-up of cars will make your car decision process
+            much faster and easier
+          </p>
         </div>
       </div>
     </main>
